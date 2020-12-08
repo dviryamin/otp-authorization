21b12a526b4956cfc4766c668938bfdd2c2868df
orel <orelrefael@gmail.com>
2020-12-08 18:13:32 +0200

    create user managment scrren

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UsersComponent } from './pages/users/users.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent
   ],
   imports: [
+    MatTableModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
---
+++ src/app/pages/users/users.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { User } from '../../models/user'
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { MembershipService } from '../../services/membership.service'
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent implements OnInit {
+
+  usersDataSource$!: Observable<MatTableDataSource<User>>;
+  displayedColumns: string[] = ['name', 'roles'];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private memberShipService: MembershipService) { }
+
+  ngOnInit(): void {
+    this.initUsers();
+  }
+
+  initUsers(): void {
+    this.usersDataSource$ = this.memberShipService.getUsers()
+      .pipe(
+        map(users => new MatTableDataSource(users)),
+        tap(dataSource => {
+          dataSource.paginator = this.paginator;
+          dataSource.sort = this.sort;
+        })
+      );
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.usersDataSource$?.pipe(tap(dataSource => {
+      dataSource.filter = filterValue.trim().toLowerCase();
+      if (dataSource.paginator) {
+        dataSource.paginator.firstPage();
+      }
+    })).subscribe();
+
+
+  }
+}
