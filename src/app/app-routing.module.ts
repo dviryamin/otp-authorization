7d13fddb5f5f1b52034d4b67bbec3083b217967c
Your Name <you@example.com>
2020-12-08 23:06:05 +0200

    roles-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
 import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component';
 import { UsersComponent } from './pages/users/users.component';
+import { RolesComponent } from './pages/roles/roles.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 
 const routes: Routes = [
   { path: '', data: { roles: [] }, canActivate: [AuthGuard], component: ProfileComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'roles', component: RolesComponent },
   { path: 'unauthorized', component: UnauthorizedComponent }
 ];
 
